4531a159855ad2be91409820da660c8470260fbd
Abdulwadud Ait El Hajj <amwadud@users.noreply.github.com>
2026-03-13 17:25:29 +0000

    Almost done

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from mazegen import parse_config, validate_config
 from mazegen import MazeGenerator
 from display import renderer_tui
@@ -10,11 +12,12 @@ def main() -> None:
         while True:
             maze.generate()
             renderer_tui.render_tui(
-                maze.grid, maze.width, maze.height, maze.locked
+                maze.grid, maze.width, maze.height, maze.locked, None, config["ENTRY"], config["EXIT"]
             )
             print("1) Regenerate: ")
-            print("1) Generate: ")
-            input("Choose: ")
+            print("1) change colors: ")
+            print("3) Quit")
+            input("Choose (based on number): ")
     except Exception as e:
          print(f"[Error]: {e}")
 
