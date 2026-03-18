591f6ba32d11cf6e503213579db5f7b0a1ad9a00
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-03-18 03:40:37 +0000

    Update

--- mazegen/data_sumbitter.py
+++ mazegen/data_sumbitter.py
@@ -1,25 +1,23 @@
 from typing import Any
 
 
-# def _get_maze_as_hex(maze: list[list[int]]) -> str:
-#     pass
-
-
-def submit_data(config: dict[str, Any], maze: list[list[int]],
-                path_directions: str):
+def submit_data(
+    config: dict[str, Any], maze: "MazeGenerator", path_directions: str
+):
     try:
         with open(config["OUTPUT_FILE"], "w") as f:
             sections = []
             maze_lines = []
-            for row in maze:
+            for row in maze.grid:
                 maze_lines.append(" ".join(hex(n) for n in row))
             sections.append("\n".join(maze_lines))
             entry = config["ENTRY"]
+
             sections.append(f"{entry[0]},{entry[1]}")
             exit_ = config["EXIT"]
             sections.append(f"{exit_[0]},{exit_[1]}")
             sections.append(path_directions)
-            f.write("\n".join(sections))
+            f.write("\n\n".join(sections))
 
     except Exception as e:
         print(f"[HMM]: {e}")
