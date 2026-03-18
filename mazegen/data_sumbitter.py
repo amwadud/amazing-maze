7a464a3598d76c00fc8ddaacc126a424ad8a7af8
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-03-18 03:48:03 +0000

    Done

--- mazegen/data_sumbitter.py
+++ mazegen/data_sumbitter.py
@@ -1,21 +1,22 @@
 from typing import Any
 
+from .generator import MazeGenerator
+
 
 def submit_data(
-    config: dict[str, Any], maze: "MazeGenerator", path_directions: str
+    config: dict[str, Any], maze: MazeGenerator, path_directions: str
 ):
     try:
         with open(config["OUTPUT_FILE"], "w") as f:
             sections = []
             maze_lines = []
             for row in maze.grid:
-                maze_lines.append(" ".join(hex(n) for n in row))
+                maze_lines.append("".join(format(n, "X") for n in row))
             sections.append("\n".join(maze_lines))
             entry = config["ENTRY"]
-
-            sections.append(f"{entry[0]},{entry[1]}")
             exit_ = config["EXIT"]
-            sections.append(f"{exit_[0]},{exit_[1]}")
+
+            sections.append(f"{entry[0]},{entry[1]}\n{exit_[0]},{exit_[1]}")
             sections.append(path_directions)
             f.write("\n\n".join(sections))
 
